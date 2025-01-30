79792c739251ef2e8ae5c917b25a6974d4c033de
derickddo <derickdanso80@yahoo.com>
2025-01-30 15:09:19 +0000

    First page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,8 @@
-import "@/styles/globals.css";
+"use client";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./globals.css";
+import { useEffect } from "react";
 import { DM_Sans } from "next/font/google";
 
 const dmSans = DM_Sans({
@@ -7,6 +11,9 @@ const dmSans = DM_Sans({
 });
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.min.js"); // Dynamically load JS
+  }, []);
   return (
     <html>
       <body className={dmSans.className}>{children}</body>
