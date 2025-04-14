5d3a9060dc523ed9e87e2834f79642d7a08b9a69
Ding-Dong101 <kwawerastus2@gamil.com>
2025-04-14 00:09:05 +0000

    update 2

--- src/app/signup/Create_artifact.js
+++ src/app/signup/Create_artifact.js
@@ -1,4 +1,7 @@
-import SideImage from "../../images/create_artifact1.png";
+import Image from "next/image";
+// import Link from "next/link";
+import sideimage from "../../Images/create_artifact1.png";
+import "../index.css";
 
 export default function Create_artifact() {
   return (
